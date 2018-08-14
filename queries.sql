10e1206184bbdc687f33c5635bbb338d9b951abf
Devinderjeet <er_devinderjeet@hotmail.com>
2018-08-14 23:28:17 +0530

    snippet_update_6

--- queries.sql
+++ queries.sql
@@ -24,6 +24,14 @@ VALUES ('dev', '12345', '1', '2');
 UPDATE `database_name`.`user` SET `user_name`='dev' WHERE `user_id`='1';
 #---------------------------------------------------
 
+# Alter
+ALTER TABLE `database_ce`.`user` 
+CHANGE COLUMN `user_name` `user_name` VARCHAR(16) NOT NULL ,
+CHANGE COLUMN `user_status` `user_email` LONGTEXT NOT NULL ,
+CHANGE COLUMN `user_key` `user_status` INT(11) NOT NULL ,
+ADD COLUMN `user_create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `user_status`;
+
+
 # Select
 SELECT * FROM `database_ce`.`user`;
 
