e411e74ec61c7f22e8756d755871c8d8bc028826
Devinderjeet <er_devinderjeet@hotmail.com>
2018-08-14 22:18:47 +0530

    snippet_update_2

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
 # Create Database
 CREATE SCHEMA `database_name` ;
-
-
-
-# Drop database
+#--------------------------------------------------
+# Delete database
 DROP SCHEMA `database_name` ;
+#--------------------------------------------------
+
+# Create Table
+CREATE TABLE `database_name`.`user` (
+  `user_id` INT NOT NULL,
+  `user_name` VARCHAR(200) NOT NULL,
+  `user_pass` VARCHAR(45) NOT NULL,
+  `user_status` INT NULL,
+  `user_key` INT NOT NULL,
+  PRIMARY KEY (`user_id`));
+#--------------------------------------------------
+# Delete Table
+DROP TABLE `database_name`.`user`;
+
+
